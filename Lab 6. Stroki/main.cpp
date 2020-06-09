6310d4e9076708810a593f09c647230df6eff313
DergunovaElena <66118811+DergunovaElena@users.noreply.github.com>
2020-06-09 19:29:41 +0500

    Add files via upload

---
+++ Lab 6. Stroki/main.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int count_digit(char* word);
+
+int main()
+{
+setlocale(LC_ALL, "rus");
+
+char s[256];
+cout << "¬ведите строку: ";
+gets_s(s);
+
+char* word = strtok(s, " ");
+while (word != NULL)
+{
+int count = count_digit(word);
+if (count == 1)
+cout << word << endl;
+
+word = strtok(NULL, " ");
+}
+}
+
+int count_digit(char* word)
+{
+int count = 0;
+
+for (int i = 0; i < strlen(word); i++)
+{
+if (isdigit(word[i]))
+count++;
+}
+
+return count;
+}
---
+++ Lab 7. Peregruzka funkcii ch.2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int sum(int k, ...)
+{
+	int* p = &k;
+	p++;
+	int res = 0;
+	for (int i = 0; i < k; i += 2)
+	{
+		res += *p * *(p + 1);
+		p += 2;
+	}
+	return res;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int sum1 = sum(8, 1, 2, 3, 4, 5, 6, 7, 8);
+	int sum2 = sum(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	int sum3 = sum(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+	cout << "Ñóììà 1 = " << sum1 << endl;
+	cout << "Ñóììà 2 = " << sum2 << endl;
+	cout << "Ñóììà 3 = " << sum3 << endl;
+}
